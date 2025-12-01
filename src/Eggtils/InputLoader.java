c712a5768b449b28b4a07a6c949e0369006a5e76
egg <mreggnoggin7@gmail.com>
2025-11-30 22:34:12 -0500

    fix

--- src/Eggtils/InputLoader.java
+++ src/Eggtils/InputLoader.java
@@ -4,12 +4,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import static java.lang.Integer.parseInt;
 
 public class InputLoader {
     public static ArrayList<String> loadInput(String filePath) {
         filePath = "src\\Inputs\\" + filePath;
-        ArrayList<String> returnStringArray = new ArrayList<String>();
+        ArrayList<String> returnStringArray = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = br.readLine()) != null) {
