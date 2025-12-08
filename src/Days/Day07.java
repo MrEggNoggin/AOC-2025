7add99c8ad90ba5e77224318940ac05bc4e57ec3
egg <mreggnoggin7@gmail.com>
2025-12-07 22:04:41 -0500

    some day 7 notes

--- src/Days/Day07.java
+++ src/Days/Day07.java
@@ -1,7 +1,12 @@
+// NOTES
+// i needed a hint from chatgpt to help me with part 1 at around 7:09 minutes, the question really confused me
+// took a break at 18:28 minutes 22:14.63 + 4:13 (<--- keeping track of time)
+
 package Days;
 
 import Eggtils.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Day07 {
 
@@ -12,10 +17,31 @@ public class Day07 {
     }
 
     public static void partOne(ArrayList<String> input) {
+        record point(int row, int col) {}
+        ArrayList<point> beams =  new ArrayList<>();
+        ArrayList<point> tempBeams = new ArrayList<>();
+        beams.add(new point(0, input.getFirst().indexOf('S')));
+        int splitCounter = 0;
 
+        for (String line : input) {
+            for (int i = 0; i < beams.size(); i++) {
+                if (line.charAt(beams.get(i).row + 1) == '.') {
+                    beams.set(i, new point(beams.get(i).row + 1, beams.get(i).col));
+                } else if (line.charAt(beams.get(i).row +1) == '^') {
+                    beams.set(i, new point(beams.get(i).row + 1, beams.get(i).col + 1));
+                    tempBeams.add(i, new point(beams.get(i).row + 1, beams.get(i).col - 1));
+                }
+            }
+        }
     }
 
     public static void partTwo(ArrayList<String> input) {
 
+    }
+
+    public static void removeDupes(ArrayList<point> points) {
+        ArrayList<Integer> indexesToRemove = new ArrayList<>();
+
+        for (int i = 0; i < points.size()) {}
     }
 }
